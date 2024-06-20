a2619e74d3dea37ad4754b689e96e2e20bb90db7
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-06-20 13:50:37 +0300

    feat/ya-map-component

--- client/src/page/MainMap/MainMap.jsx
+++ client/src/page/MainMap/MainMap.jsx
@@ -1,21 +1,61 @@
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import "./MainMap.css";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import {
+  YMaps,
+  Map,
+  Placemark,
+  RouteButton,
+  RouteEditor,
+  RoutePanel,
+  GeolocationControl,
+} from "@pbe/react-yandex-maps";
 
 function MainMap() {
-  const [zoom, setZoom] = useState(9);
-  const mapState = useMemo(
-    () => ({ center: [59.936402, 30.304871], zoom }),
-    [zoom]
-  );
+  const map = useRef(null);
+  const mapState = {
+    center: [55.739625, 37.5412],
+    zoom: 12,
+  };
+
+  const addRoute = (ymaps) => {
+    const pointA = [60.327882, 30.327952]; 
+    const pointB = [60.366956, 30.320103]; 
+
+    const multiRoute = new ymaps.multiRouter.MultiRoute(
+      {
+        referencePoints: [pointA, pointB],
+        params: {
+          routingMode: "bicycle",
+        },
+      }, 
+      {
+        boundsAutoApply: true,
+        zoomMargin: 20
+      }
+    );
+
+    map.current.geoObjects.add(multiRoute);
+  };
+
   return (
     <div>
-      <YMaps>
-        <Map
+      <YMaps query={{ apikey: "792bbe16-8675-412c-8807-6065062e8b6f" }}>
+        {/* <Map
           id="map"
           className="map"
           state={mapState}
-          modules={["control.ZoomControl", "control.FullscreenControl"]}
+          instanceRef={map}
+          onLoad={addRoute}
+        //   defaultState={{
+        //     center: [59.936402, 30.304871],
+        //     zoom: 9,
+        //     controls: [],
+        //   }}
+          modules={[
+            "control.ZoomControl",
+            "control.FullscreenControl",
+            "multiRouter.MultiRoute",
+          ]}
         >
           <Placemark
             modules={["geoObject.addon.balloon"]}
@@ -25,12 +65,18 @@ function MainMap() {
                 "This is balloon loaded by the Yandex.Maps API module system",
             }}
           />
-        </Map>
-        <p>
-          <button onClick={() => setZoom((zoom) => (zoom === 9 ? 12 : 9))}>
-            Toggle map zoom
-          </button>
-        </p>
+          <GeolocationControl options={{ float: "left" }} />
+          <RouteButton options={{ float: "right" }} /> 
+          <RouteEditor />
+          <RoutePanel options={{ float: "right" }} />
+        </Map> */}
+        <Map
+          className="map"
+          modules={["multiRouter.MultiRoute"]}
+          state={mapState}
+          instanceRef={map}
+          onLoad={addRoute}
+        ></Map>
       </YMaps>
     </div>
   );
