a05e907d31daf82796868f9b4e8bd374d5921b9e
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-06-20 17:33:47 +0300

    style/main-page

--- client/src/page/main/Main.jsx
+++ client/src/page/main/Main.jsx
@@ -4,6 +4,7 @@ function Main() {
   return (
     <div>
       <h1 className='main-page-text'>Main Page</h1>
+      <img className="main-oage-image" src="/img/photo_2024-06-20_17-14-35.jpg" />
     </div>
   );
 }
