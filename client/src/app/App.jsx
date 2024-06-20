167dca01754aa2789e0e8523d7d45fdd95c12e9c
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-06-20 14:52:31 +0300

    feat/navbar

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,16 +1,41 @@
 import "./App.css";
 import { Loader } from "../ui/Loader/Loader";
-import MainMap from "../page/MainMap/MainMap";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../page/navbar/Navbar";
+import MainMap from "../page/mainMap/MainMap";
+import Main from "../page/main/Main";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Hello!</h1>
-      <MainMap />
-      <div className="main-loader">
-        <Loader />
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div>
+          <Navbar />
+          <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/routes" element={<MainMap />} />
+            <Route path="/map" element={<MainMap />} />
+          </Routes>
+        </div>
+      ) : (
+        <div className="main-loader">
+          <Loader />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ client/src/page/main/Main.jsx
@@ -0,0 +1,11 @@
+import './main.css';
+
+function Main() {
+  return (
+    <div>
+      <h1 className='main-page-text'>Main Page</h1>
+    </div>
+  );
+}
+
+export default Main;
